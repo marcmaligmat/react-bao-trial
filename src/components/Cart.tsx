17e6f8e81b9d1f822b0b7eea1477d9d6c3f3ea25
Marc <marcmaligmat@gmail.com>
2023-02-22 07:56:51 +0800

    added loading

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -34,30 +34,39 @@ const Cart = () => {
   }
   return (
     <>
-      {state.cartItems.map((item) => {
-        return (
-          <div key={item.name} className="border border-gray-200 ">
-            <div className="flex flex-col justify-between p-5">
-              <div key={item.name} className="flex justify-between">
-                <div className="w-3/4">{item.name}</div>
-                <div className="flex items-center justify-center w-1/4 ">
-                  <RemoveIcon
-                    onClick={() => handleMinusIcon(item.name)}
-                    className="mx-3 rounded cursor-pointer hover:bg-red-300"
-                  />
-                  <div className="flex justify-center w-4 px-4 rounded bg-slate-200">
-                    {item.quantity}
+      {state.cartItems.length > 0 ? (
+        state.cartItems.map((item) => {
+          return (
+            <div key={item.name} className="border border-gray-200 ">
+              <div className="flex flex-col justify-between p-5">
+                <div key={item.name} className="flex justify-between">
+                  <div className="w-3/4">{item.name}</div>
+                  <div className="flex items-center justify-center w-1/4 ">
+                    <RemoveIcon
+                      onClick={() => handleMinusIcon(item.name)}
+                      className="mx-3 rounded cursor-pointer hover:bg-red-300"
+                    />
+                    <div className="flex justify-center w-4 px-4 rounded bg-slate-200">
+                      {item.quantity}
+                    </div>
+                    <AddIcon
+                      onClick={() => handleAddIcon(item.name)}
+                      className="mx-3 rounded cursor-pointer hover:bg-green-300"
+                    />
                   </div>
-                  <AddIcon
-                    onClick={() => handleAddIcon(item.name)}
-                    className="mx-3 rounded cursor-pointer hover:bg-green-300"
-                  />
                 </div>
               </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })
+      ) : (
+        <div className="flex flex-col gap-2 items-center justify-center bg-no-repeat border-slate-500 h-[400px]">
+          <div
+            style={{ backgroundImage: "url(./image1.svg)", width: "60px", height: "60px" }}
+          ></div>
+          <span className="inline-block">No product has benn added</span>
+        </div>
+      )}
     </>
   )
 }
--- src/db/fruits.js
+++ src/db/fruits.js
@@ -1,17 +1,7 @@
 export const fetchFruits = async () => {
-  const response = await fetch(
-    "https://cors-anywhere.herokuapp.com/https://fruityvice.com/api/fruit/all"
-  )
+  const response = await fetch("https://fruityvice.com/api/fruit/all")
   if (!response.ok) {
     throw new Error("Something went wrong!")
   }
   return response.json()
 }
-// ,
-//     {
-//       headers: {
-//         "Access-Control-Allow-Origin": "http://localhost:3001",
-//         "Content-Type": "application/json",
-//       },
-//       allowedHeaders: { "Access-Control-Allow-Origin": "*" },
-//     }
